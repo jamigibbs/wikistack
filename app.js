8a56661d2412bab643b35f46c334e8e988e64c37
aurre <aurrecochearaysa@gmail.com>
2018-04-27 10:24:09 -0500

    server running

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(morgan('dev'));
 
 const staticMiddleware = express.static(path.join(__dirname, 'public'));
 app.use(staticMiddleware);
 
+app.get('/', function (req, res, next) {
+    res.send('Hello World');
+})
+
 app.listen(3000, function() {
  console.log('Listeting in port 3000');
 })
+
