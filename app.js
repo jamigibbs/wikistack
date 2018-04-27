e35eebcd23e6f0c61e6d2b8ff23c0a5291418dc8
Jami Gibbs <jami0821@gmail.com>
2018-04-27 11:01:30 -0500

    Adding views and postgres

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
+const { db } = require('./models');
+const layout = require('./views/layout');
 
 app.use(morgan('dev'));
 
 const staticMiddleware = express.static(path.join(__dirname, 'public'));
 app.use(staticMiddleware);
 
-app.get('/', function (req, res, next) {
-    res.send('Hello World');
+db.authenticate().
+then( () => {
+  console.log('Connected to the database');
 })
 
-app.listen(3000, function() {
- console.log('Listeting in port 3000');
+app.get('/', function (req, res, next) {
+    res.send(layout('We got it working!!'));
+});
+
+app.listen(5432, function() {
+ console.log('Listeting in port 5432');
 })
 
